9cec08be28e4f87fc663659545112f8a32a8c0fa
EncodingAlba <1578985@alu.murciaeduca.es>
2025-09-24 14:29:00 +0200

    Update Hilo.java

--- UT02/EjemploHiloConExcepcion/src/Hilo.java
+++ UT02/EjemploHiloConExcepcion/src/Hilo.java
@@ -21,7 +21,7 @@ class Hilo implements Runnable {
     }
 }
 
-public class LanzaHilosYEsperaQueTerminen {
+class LanzaHilosYEsperaQueTerminen {
     public static void main(String[] args) {
         Thread h1 = new Thread(new Hilo("H1"));
         Thread h2 = new Thread(new Hilo("H2"));
