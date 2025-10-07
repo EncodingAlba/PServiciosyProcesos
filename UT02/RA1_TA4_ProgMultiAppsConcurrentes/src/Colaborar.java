36121519b9ff48b08af7023df891902fd3403215
EncodingAlba <1578985@alu.murciaeduca.es>
2025-10-07 12:46:18 +0200

    update

--- UT02/RA1_TA4_ProgMultiAppsConcurrentes/src/Colaborar.java
+++ UT02/RA1_TA4_ProgMultiAppsConcurrentes/src/Colaborar.java
@@ -1,10 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author Alba
- * @since 01.10.25
+ * @author Alba Muñoz Miñano
+ * @since 07.10.25
  * @version 1.0
  */
 public class Colaborar {
     public static void main(String[] args) {
+String fichero = "miFicheroDeLenguaje.txt";
+int numProcesos = 10;
+int numLetrasPorLinea = 10;
+int incrementoLineas = 10;
+
+        //Iniciamos una lista para almacenar los hilos
+        List<Thread> hilos = new ArrayList<>();
+
+        //Creamos y lanzamos los hilos
+        for (int i = 1; i <= numProcesos; i++) {
+            //Número de líneas por hacer en cada instancia
+            int lineasPorHacer =i * incrementoLineas;
+
+            //Creamos el proceso (o tarea) del objeto Lenguaje
+            Lenguaje lenguaje = new Lenguaje(fichero, lineasPorHacer, numLetrasPorLinea);
+
+            //Creamos el hilo para ejecutar la tarea
+            Thread thread = new Thread(lenguaje);
+            hilos.add(thread);
+            thread.start();
+        }
+
+        //Esperamos a que todos los hilos terminen
+        for (Thread thread : hilos) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Error al esperar el hilo: " + e.getMessage());
+            }
+
+        }
 
     }
 }
